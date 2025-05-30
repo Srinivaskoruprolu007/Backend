cf4014f047fc9760adb8e40b238417c8070ea9d2
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-05-30 14:49:19 +0530

    feat: enhance query performance by adding timing logs and ensuring secret tours are excluded in find and aggregate operations

--- express/natours/models/tourModel.js
+++ express/natours/models/tourModel.js
@@ -89,10 +89,26 @@ tourSchema.post('save', function (doc, next) {
   next();
 });
 
-tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
     this.find({secretTour : {$ne : true}})
+    this.start = Date.now();
     next()
 });
+
+tourSchema.post(/^find/, function (docs, next) {
+    console.log(`Query took  ${Date.now() - this.start} milliseconds`);
+    next();
+});
+
+// Aggregation middleware
+
+tourSchema.pre('aggregate', function(next){
+    this.pipeline().unshift({
+        $match: {secretTour : {$ne : true} }
+    })
+    console.log(this.pipeline());
+    next()
+})
 
 const Tour = mongoose.model('Tour', tourSchema);
 
