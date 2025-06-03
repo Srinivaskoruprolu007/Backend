24bebb7d4dd383c69d7c44ae81320fa62750792e
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-06-03 19:31:50 +0530

    refactor: move 404 error handler to the end of the middleware stack for proper routing

--- express/natours/app.js
+++ express/natours/app.js
@@ -11,12 +11,7 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `can't find ${req.originalUrl} on this server`,
-  });
-});
+
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -61,6 +56,13 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `can't find ${req.originalUrl} on this server`,
+  });
+});
 
 // 4) Start the server
 export default app;
