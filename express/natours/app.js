0f7df51983542154b8f538460d8225630cbd87d6
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-06-04 17:37:28 +0530

    refactor: clean up app.js for improved readability and organization

--- express/natours/app.js
+++ express/natours/app.js
@@ -58,11 +58,31 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `can't find ${req.originalUrl} on this server`,
-  });
+//   res.status(404).json({
+//     status: 'fail',
+//     message: `can't find ${req.originalUrl} on this server`,
+//   });
+
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`)
+    err.status = 'fail'
+    err.statusCode = 400
+    next(err)
 });
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status : err.status,
+        message : err.message
+
+    })
+
+
+    res.status()
+} )
+
 
 // 4) Start the server
 export default app;
